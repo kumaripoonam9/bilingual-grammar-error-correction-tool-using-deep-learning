ffbb84a8818a17a8d322bd2d4164aa7982cee0d2
KUMARI POONAM <kumari.p@somaiya.edu>
2022-11-24 22:48:35 +0530

    initial commit
    django app
    - grammar check done
    
    documents
    - srs
    - scope
    - spmp
    
    ui html

---
+++ django app/gectool/gectool/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('userauth/', include('userauth.urls')),
+    path('grammarcheck/', include('grammarcheck.urls')),
+    path('spellcheck/', include('spellcheck.urls')),
+    path('summarizer/', include('summarizer.urls')),
+]
